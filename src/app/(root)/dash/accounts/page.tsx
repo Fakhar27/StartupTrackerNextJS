e9d4adfc9a3e9192d4c34c9e3748150be6ec03fd
Fakhar27 <fakharmursaleen12345@gmail.com>
2025-02-26 17:06:35 +0500

    dev

---
+++ src/app/(root)/dash/accounts/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const accounts = () => {
+  return (
+    <div>
+      <h4>THIS IS ACCOUNTS PAGE</h4>
+    </div>
+  )
+}
+
+export default accounts
---
+++ src/app/(root)/dash/users/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Users = () => {
+  return (
+    <div>
+       <h2>THIS IS USERS PAGE SELECT YOUR USERS</h2>
+       <ul>
+            <li>
+                <link rel="stylesheet" href="/app/root/dash/users/1" />
+            </li>
+       </ul>
+    </div>
+  )
+}
+
+export default Users
